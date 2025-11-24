1f6081ad67e7dcbfe7119f76e05751b4a698804c
Mykola <masiuk.mykola@globaldev.tech>
2025-11-24 13:15:22 +0200

    refactor: simplify URL generation in handleSocialLogin by removing redundant getURL function

--- src/services/supabase/components/login-form.tsx
+++ src/services/supabase/components/login-form.tsx
@@ -16,24 +16,11 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
     setIsLoading(true);
     setError(null);
 
-    const getURL = () => {
-      let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-        'http://localhost:3000/';
-      // Make sure to include `https://` when not localhost.
-      url = url.startsWith('http') ? url : `https://${url}`;
-      // Make sure to include a trailing `/`.
-      url = url.endsWith('/') ? url : `${url}/`;
-      return url;
-    };
-
     try {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'github',
         options: {
-          // redirectTo: `${window.location.origin}/auth/oauth?next=/`,
-          redirectTo: `${getURL()}auth/oauth?next=/`,
+          redirectTo: `${window.location.origin}/auth/oauth?next=/`,
         },
       });
 
